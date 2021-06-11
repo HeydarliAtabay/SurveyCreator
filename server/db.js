952903308fbc6ddc8b518b8873e935340f7cf7db
Atabay Heydarli <heydarli.atabay@gmail.com>
2021-06-11 16:49:02 +0200

    db and server dependencies were added

---
+++ server/db.js
@@ -0,0 +1,10 @@
+'use strict';
+
+const sqlite = require('sqlite3');
+
+// open the database
+const db = new sqlite.Database('survey.db', (err) => {
+  if (err) throw err;
+});
+
+module.exports = db;
