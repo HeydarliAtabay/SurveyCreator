174846fdc12ea7c50fad598a381692d8af1ad12d
Atabay Heydarli <heydarli.atabay@gmail.com>
2021-06-23 21:56:42 +0200

    User APIs were added

--- client/src/API.js
+++ client/src/API.js
@@ -352,9 +352,56 @@ function addQuestion(question,orderId, surveyId) {
   }
 
 
+   // User APIs
+  // Login
+
+  async function logIn(credentials) {
+    let response = await fetch('/api/sessions', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(credentials),
+    });
+    if(response.ok) {
+      const user = await response.json();
+      return user.name;
+    }
+    else {
+      try {
+        const errDetail = await response.json();
+        throw errDetail.message;
+      }
+      catch(err) {
+        throw err;
+      }
+    }
+  }
+  
+  // logout
+  async function logOut() {
+    await fetch('/api/sessions/current', { method: 'DELETE' });
+  }
+  
+
+  // getting user info 
+  async function getUserInfo() {
+    const response = await fetch(url + '/api/sessions/current');
+    const userInfo = await response.json();
+    if (response.ok) {
+      return userInfo;
+    } else {
+      throw userInfo;  // an object with the error coming from the server
+    }
+  }
+
+
+
     //Error handling is missing
 const API={loadAllSurveys, deleteSurvey, addSurvey, publishSurvey,increaseNumRespond,
    addQuestion, loadAllQuestions, deleteQuestion, getQuestions, updateOrderQuestion, 
    getAnswers, addEmptyAnswers, updateClosedAnswers, updateOpenAnswers, updateStatusAnswer, deleteAnswer,
-   getSubmissionsOfSurvey, addNewSubmission}
+   getSubmissionsOfSurvey, addNewSubmission,
+  logIn,logOut,getUserInfo,
+  }
 export default API
