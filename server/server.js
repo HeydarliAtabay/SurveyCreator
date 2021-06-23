02d1b3c806392fd4fdca2e08fec569c33641e734
Atabay Heydarli <heydarli.atabay@gmail.com>
2021-06-23 21:49:57 +0200

    Authentication configuration and User APIS were added to the server

--- server/server.js
+++ server/server.js
@@ -5,9 +5,13 @@ const app = new express();
 const PORT = 3001;
 const morgan = require('morgan');
 
-
+const userDao = require('./userDao') // module for accessing the DB of users
 const surveyDao = require('./surveyDao'); // module for accessing the DB for surveys
 const questionDao = require('./questionDao'); // module for accessing the DB for questions
+
+const passport = require('passport')
+const LocalStrategy = require('passport-local').Strategy; // username and password for login
+const session= require('express-session')
 
 app.use(morgan('dev'))
 app.use(express.json())
@@ -15,6 +19,57 @@ app.use(express.json())
 app.get('/',(req, res)=>{
     res.send(`Hi from the server, which is running on  http://localhost:${PORT}/`)
 })
+
+// for user authentication
+
+passport.use(new LocalStrategy(
+  function(username, password, done) {
+    userDao.getUser(username, password).then((user) => {
+      if (!user)
+        return done(null, false, { message: 'Incorrect username and/or password.' });
+        
+      return done(null, user);
+    }).catch(err=>{
+        done(err)
+    })
+  }
+));
+
+// serialize and de-serialize the user (user object <-> session)
+// we serialize the user id and we store it in the session: the session is very small in this way
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+// starting from the data in the session, we extract the current (logged-in) user
+passport.deserializeUser((id, done) => {
+  userDao.getUserById(id)
+    .then(user => {
+      done(null, user); // this will be available in req.user
+    }).catch(err => {
+      done(err, null);
+    });
+});
+
+// checking whether the user is authenticated or not
+const isLoggedIn = (req, res, next) => {
+  if(req.isAuthenticated())
+    return next();
+  
+  return res.status(401).json({ error: 'not authenticated'});
+}
+
+// set up the session
+app.use(session({
+  // by default, Passport uses a MemoryStore to keep track of the sessions
+  secret: 'Ondan ona ondan ona, cay verun limonnan ona',
+  resave: false,
+  saveUninitialized: false 
+}));
+
+// tell passport to use session cookies
+app.use(passport.initialize())
+app.use(passport.session())
 
 // for Surveys
 
@@ -280,6 +335,56 @@ app.put('/api/answers/updateopen/:question/:submission', async (req, res) => {
       .catch((err) => res.status(500).json(`Error while deleting the task with id:${req.params.questionId}  `+err),
       );
 });  
+
+
+/*** Users APIs ***/
+
+// POST /sessions 
+// login
+app.post('/api/sessions', function(req, res, next) {
+  passport.authenticate('local', (err, user, info) => {
+    if (err)
+      return next(err);
+      if (!user) {
+        // display wrong login messages
+        return res.status(401).json(info);
+      }
+      // success, perform the login
+      req.login(user, (err) => {
+        if (err)
+          return next(err);
+        
+        // req.user contains the authenticated user, we send all the user info back
+        // this is coming from userDao.getUser()
+        return res.json(req.user);
+      });
+  })(req, res, next);
+});
+
+// ALTERNATIVE: if we are not interested in sending error messages...
+/*
+app.post('/api/sessions', passport.authenticate('local'), (req,res) => {
+  // If this function gets called, authentication was successful.
+  // `req.user` contains the authenticated user.
+  res.json(req.user);
+});
+*/
+
+// DELETE /sessions/current 
+// logout
+app.delete('/api/sessions/current', (req, res) => {
+  req.logout();
+  res.end();
+});
+
+// GET /sessions/current
+// check whether the user is logged in or not
+app.get('/api/sessions/current', (req, res) => {
+  if(req.isAuthenticated()) {
+    res.status(200).json(req.user);}
+  else
+    res.status(401).json({error: 'Unauthenticated user!'});;
+});
 
 
 
