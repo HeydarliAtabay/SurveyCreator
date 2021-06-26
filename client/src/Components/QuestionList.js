d3c59273f1808af944a4dc593d7fe109e099109b
Atabay Heydarli <heydarli.atabay@gmail.com>
2021-06-26 13:10:15 +0200

    Changing a state of save open questions both from question and by pressing submit

--- client/src/Components/QuestionList.js
+++ client/src/Components/QuestionList.js
@@ -1,4 +1,4 @@
-import { React, useState} from "react";
+import { React, useState, useEffect} from "react";
 import { ListGroup, Button, Form, Row, Col, Container} from "react-bootstrap";
 import {
   BookmarkStar,
@@ -12,27 +12,64 @@ import API from '../API'
 let abc=[] // array for storing question ids, which then will be used for submitting the answers
 
 function QuestionItem(props) {
-  const { question, onDelete, onUp, onDown, index,last, setAnswers, survey, submission, numberOfQuestions, logged} = props;
+  const { question, onDelete, onUp, onDown, index,last, setAnswers, survey, submission, numberOfQuestions, logged, newCheck, setNewCheck, openCheck} = props;
   const [answered, setAnswered]=useState([false,false,false,false,false,false,false,false,false,false])
   const [openQAnswer, setOpenQAnswer]=useState('')
-  const [error, setError]=useState(true)
   const [message, setMessage]=useState({
     text: '',
     type: 'yes',
   })
   const [count, setCount] = useState(0)
+  const [errArr, setErrArr]=useState(false)
   const [addEmpty, setAddEmpty]=useState(false)
   const [saveOpen, setSaveOpen]=useState(false)
-  
   let newSubId=(submission[submission.length-1].id)+1 // new id of the new submission
-  
+ 
+  console.log('open check is '+ openCheck)
+
+
+  useEffect(() => {
+    if(openCheck){
+     setSaveOpen(true)
+     const newAnswer=Object.assign({},  { answer: openQAnswer  })
+    API.updateOpenAnswers(newAnswer, question.id ,newSubId)
+    .then(()=>{
+    }).catch(err=>(err))
+     console.log("save open state was changed")
+      }
+    
+    }, [openCheck])
   // styles for different message types
   const okayStyle = {color: "green" }
   const noStyle={color:"red"}
-  
+  const id=index
+
+  function changeErrorCheck (value){
+    // if(value===true){
+    //   let items=[...newCheck]
+    //   console.log('items are '+ items)
+    //   let item = items[id]
+    //   console.log('item: '+ item)
+    //   items[id]=true
+    //   setError(true)
+    //   setNewCheck(items)
+    //   setMessage({text:`You can submit minimum ${question.min} and maximum ${question.max} answers`, type:'no'})  
+    // }
+    // else{
+    //   let items=[...newCheck]
+    //   console.log('items are '+ items)
+    //   let item = items[id]
+    //   console.log('item: '+ item)
+    //   items[id]=false
+    //   setError(false)
+    //   setNewCheck(items)
+    //   setMessage({text:'All specifications are done', type:'okay'})
+    // }
+    
+   }
+
 
   let num=count // counter for counting the number of selected answers
-
   // function for adding empty answers, which will be updated after
   async function addEmptyAnswers(question1){
 
@@ -59,7 +96,9 @@ function QuestionItem(props) {
   }).catch(err=>(err))
 }
 
+
 let control=count // defining control integer for adding empty answer for opened questions only once, otherwise if typing very fast it adds some useless lines 
+
 function onChangeOpenAnswer(ev){
 control++
 setCount(control)
@@ -68,7 +107,7 @@ if(count===1){
  
 } 
 setOpenQAnswer(ev.target.value)  // changing state of open answer, to the value of the form input control
-  
+changeErrorCheck(false)
 }
 
 // function for adding the result of open answer to the DB
@@ -89,16 +128,19 @@ function onChangeSaveOpen(ev, questionId){
 let checkedAnswers=answered
 let finalanswers=[0,0,0,0,0,0,0,0,0,0]
 
+
+
 // function for adding answers and specifications of closed questions
 async function onChangeAnswer (ev,question,index) {
+  
   await addEmptyAnswers(question) // adding empty answer only once
+  setErrArr(true)
   if(ev.target.checked)  {
     num++ // increasing number of selected answers after checking the answer
     setCount(num) 
-    
+   
     if(num>=question.min && num<=question.max){
-    setError(false)
-    setMessage({text:'All specifications are done', type:'okay'})
+      changeErrorCheck(false)
     checkedAnswers[index]=true
     setAnswered(checkedAnswers)
     for(let i=0;i<10;i++){
@@ -110,8 +152,8 @@ async function onChangeAnswer (ev,question,index) {
 
     }
     if (num<question.min || num>question.max) {
-      setError(true)
-      setMessage({text:`You can submit minimum ${question.min} and maximum ${question.max} answers`, type:'no'})  
+      changeErrorCheck(true)
+       
       checkedAnswers[index]=true
     setAnswered(checkedAnswers)
     for(let i=0;i<10;i++){
@@ -130,9 +172,8 @@ async function onChangeAnswer (ev,question,index) {
     setCount(num)
    }
     if(num<=question.min || num>=question.max){
-      setError(true)
-      setMessage({text:`You can submit minimum ${question.min} and maximum ${question.max} answers`, type:'no'}) 
-      checkedAnswers[index]=false
+     changeErrorCheck(true)
+      checkedAnswers[index]='false'
      setAnswered(checkedAnswers)
       for(let i=0;i<10;i++){
        if(checkedAnswers[i]===false) finalanswers[i]=0
@@ -142,8 +183,7 @@ async function onChangeAnswer (ev,question,index) {
     await updateClosedAnswers(newAnswer, question.id)
     }
     if(num>=question.min && num<=question.max) {
-     setError(false)
-     setMessage({text:'All specifications are done', type:'okay'})
+      changeErrorCheck(false)
      checkedAnswers[index]=false
      setAnswered(checkedAnswers)
       for(let i=0;i<10;i++){
@@ -354,14 +394,23 @@ function QuestionRowControl(props) {
 
 function QuestionList(props) {
   let number=0
+  let errorArray=[]
   const history=useHistory()
   const { questions, onDelete, onUp, onDown, onPublish, survey, submission, loading, logged} = props;
   const [name, setName]=useState('')
   const [answers, setAnswers]=useState([])
+  const [newCheck, setNewcheck]=useState([false,false,true,false,true,false,false])
+  const [openCheck, setOpenCheck]=useState(false)
   const [validated, setValidated] = useState(false);
-  
-  let newSubId=(submission[submission.length-1].id)+1
+  const [errorMessage, setErrorMessage]= useState({
+    text: '',
+    type: 'okay'
+  })
+  const okayStyle = {color: "green" }
+  const noStyle={color:"red"}
 
+  console.log('new check: ' + newCheck)
+  let newSubId=(submission[submission.length-1].id)+1
   function newSubmission(){
     if(name && survey){
       API.addNewSubmission(name, survey).then((err)=>{})
@@ -376,23 +425,48 @@ function QuestionList(props) {
     // stop event default and propagation
     event.preventDefault();
     event.stopPropagation(); 
-
-   
+    setOpenCheck(true)
+    let valid=true
     const form = event.currentTarget; 
-
-    // check if form is valid using HTML constraints
-    if (!form.checkValidity()) { 
-      setValidated(true); // enables bootstrap validation error report
+    for(let i=0;i<newCheck.length;i++){
+      if(newCheck[i]===true) valid=false
     }
-     
+    console.log("is form valid: " + valid)
+    // check if form is valid using HTML constraints
+    if (!form.checkValidity() ) { 
+        setValidated(true); // enables bootstrap validation error report
+        setErrorMessage({text:`You can not submit your responses, please check the specifications `, type:'no'})  
+    }
     else {
-      console.log("Everything is fine")
-      newSubmission()
-      history.push('/surveys')
+      if(valid===true){
+        setErrorMessage({text:`Your answer is submitted `, type:'okay'}) 
+        console.log("Everything is fine")
+      }
+      
+     // newSubmission()
+     // history.push('/surveys')
+    //  errorArray.length=0
+
+    }
+
+    if(valid===false){ // enables bootstrap validation error report
+      setErrorMessage({text:`You can not submit your responses, please check the specifications `, type:'no'})  
     }
  
 
   }
+  // **************************bidene burdan errorArrayi setstate kimi newChecke atsam duzeler*****************************
+  //********************************************************************************************************************** */
+  // let num=questions.length
+  // for(let i=0;i<num;i++) errorArray.push(0)
+  // console.log(errorArray)
+  // for(let i=0;i<errorArray.length;i++){
+  //   if(questions[i].min!==0 && questions[i].questiontype===1)errorArray[i]=true
+  //   else errorArray[i]=false
+  // }
+  // console.log(errorArray)  
+
+
   let last=questions.length
   let surveyId=0
   last!==0?surveyId=questions[0].survey_id: surveyId=0
@@ -402,30 +476,36 @@ function QuestionList(props) {
       <div className="cont">
 
          <Form noValidate validated={validated} onSubmit={handleSubmit}>  
+       {!logged &&
+       <>
         <h3>
-          Please, Write your name and start answering questions for the survey
-        </h3>
-        <Form.Group controlId="validationUsername">
-        <Row>
-          <Col sm={4}>
-            <h3>Your name:</h3>
-          </Col>
-          <Col sm={8}>
-            <Form.Control hasValidation size="lg" type="text" placeholder="Write your name"   value={name}
-                onChange={(ev) => setName(ev.target.value)}
-                required/>
-                <Form.Text className="text-muted"> You don't have to write your exact name, any username is accepted</Form.Text>
-                 
-          </Col>
-          
-        </Row>
-        </Form.Group>
+        Please, Write your name and start answering questions for the survey
+      </h3>
+      <Form.Group controlId="validationUsername">
+      <Row>
+        <Col sm={4}>
+          <h3>Your name:</h3>
+        </Col>
+        <Col sm={8}>
+          <Form.Control hasValidation size="lg" type="text" placeholder="Write your name"   value={name}
+              onChange={(ev) => setName(ev.target.value)}
+              required/>
+              <Form.Text className="text-muted"> You don't have to write your exact name, any username is accepted</Form.Text>
+               
+        </Col>
+        
+      </Row>
+      </Form.Group>
+       </>
+       }
+       
         <Form.Group>
           {loading ?<h3>Please wait for your questions loading</h3> :
           <>
  <ListGroup as="ul" variant="flush" key={questions.id}>
           {questions.map((q,index) => {
             number++
+              
             return (
               <>
               
@@ -442,6 +522,9 @@ function QuestionList(props) {
                     setAnswers={setAnswers}
                     numberOfQuestions={questions.length}
                     logged={logged}
+                    newCheck={newCheck}
+                    setNewCheck={setNewcheck}
+                    openCheck={openCheck}
                   />
                 </ListGroup.Item>
               </>
@@ -454,7 +537,8 @@ function QuestionList(props) {
         </Form.Group>
 {(number===0 && logged) && <h4>Number of questions is {number}. You should have at least one question for publishing the survey</h4>}
  {(number!==0 && logged) && <h6>The number of questions is {number}. It is enough for publishing. Good Luck</h6> }
-     
+       {errorMessage.type==="okay" && <Row><span style={okayStyle}>{errorMessage.text}</span></Row>} 
+       {errorMessage.type!=="okay" && <Row><span style={noStyle}>{errorMessage.text}</span></Row>} 
        {number!==0 &&
        <Button  variant="success" type="submit">
        Submit the answers
@@ -472,6 +556,7 @@ function QuestionList(props) {
           Publish this survey
         </Button>
         }
+        
         </Form>
       </div>
     </>
